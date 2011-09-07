67ae8ff521318092db57d02f1ef1444c91b20bf8
Graham Alldredge <gramathy@gmail.com>
2011-09-06 17:27:42 -0700

    stuff

--- connectionManager.py
+++ connectionManager.py
@@ -2,6 +2,7 @@
 
 from esUtils import *
 from esModels import *
+from django.utils import simplejson as json
 from google.appengine.ext import webapp
 from google.appengine.api import channel
 from google.appengine.ext.webapp.util import run_wsgi_app	
@@ -18,12 +19,14 @@ class CHandler(webapp.RequestHandler):
 		room = client[0]
 		user = db.get(db.Key.from_path('Player', client[1])) # Key object from client ID used to locate corresponding datastore entry
 		user.room = room;
+		logging.info(room)
 		user.put()
 		content = dict()
-		content['name'] = nickname
+		content['name'] = user.user.nickname()
 		roomquery = Player.all()
 		roomquery.filter('room =', room)
 		for player in roomquery:
+			logging.info(player.user.user_id())
 			channel.send_message(" ".join([room, player.user.user_id()]), 
 				buildJSONMessage('connect', content))
 
@@ -35,37 +38,44 @@ class DCHandler(webapp.RequestHandler):
 	def post(self):
 		client_id = self.request.get('from')
 		client = client_id.split(None,1)
+		logging.info(client_id)
 		room = client[0]
 		user = db.get(db.Key.from_path('Player', client[1]))
-		user.room = '';
+		# user.room = '';
 		user.put()
 		content = dict()
-		content['name'] = nickname
+		content['name'] = user.user.nickname()
 		roomquery = Player.all()
 		roomquery.filter('room =', room)
 		for player in roomquery:
 			channel.send_message(" ".join([room, player.user.user_id()]), 
 				buildJSONMessage('disconnect', content))
 				
-class RCHandler(webapp.RequestHandler):
+class TokenHandler(webapp.RequestHandler):
 	def get(self):
 		return
 		
 	def post(self):
-		room = self.request.get('from')
+		msgJSON = self.request.get('json')
+		logging.info(str(msgJSON))
+		msgobj = json.JSONDecoder().decode(msgJSON)
+		if (msgobj["type"] != "system"): return
+		room = msgobj["content"]["room"]
 		user = get_user()
 		user.room = room
+		user.put()
 		token = channel.create_channel(" ".join([room, user.user.user_id()]))
 		content = dict()
 		content['token'] = token
-		self.response.out.write(buildJSONMessage('reconnect', content))
+		logging.info(token)
+		self.response.out.write(buildJSONMessage('connect', content))
 		
 def main():
     app = webapp.WSGIApplication(
 		[
 			(r'/_ah/channel/connected/', CHandler),
 			(r'/_ah/channel/disconnected/', DCHandler),
-			(r'/tokenrequest', RCHandler)
+			(r'/tokenrequest', TokenHandler)
 		],
         debug=True)
     run_wsgi_app(app)
